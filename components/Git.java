a9e24b26cdda3fc33eae0326c99eb6ae71e7201c
charbim <cphelan1@hwemail.com>
2025-10-17 17:35:29 -0700

    Added a "commit history" to Git class.
    hopefully to streamline checkout when I figure out how the working list works.

--- components/Git.java
+++ components/Git.java
@@ -1,10 +1,16 @@
 package components;
 
 import java.io.File;
+import java.util.HashMap;
+
+import functions.CommitFile;
 
 public class Git {
 
     private File git;
+
+    // Stores all commits.
+    private HashMap<String, CommitFile> commitHistory;
 
     public Git(String pathname) {
         initializePath(pathname);
@@ -35,6 +41,14 @@ public class Git {
 
     public boolean delete() {
         return git.delete();
+    }
+
+    public void updateCommitHistory(CommitFile commit) {
+        commitHistory.put(commit.getHash(), commit);
+    }
+
+    public CommitFile findCommit(String commitHash) {
+        return commitHistory.get(commitHash);
     }
 
 }
